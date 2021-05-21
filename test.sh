4a94f6c0412e9c94e0e841b83ec09e7270ce30d1
Mariano Conti <nanexcool@gmail.com>
2021-05-20 22:01:44 -0300

    Run all tests

--- test.sh
+++ test.sh
@@ -4,5 +4,5 @@ export DAPP_SOLC=/usr/bin/solc
 export DAPP_BUILD_OPTIMIZE=1
 export DAPP_BUILD_OPTIMIZE_RUNS=999999
 # dapp build
-dapp test --verbosity 2 -m gas_usage
+dapp test --verbosity 2 # -m gas_usage
 # dapp debug
